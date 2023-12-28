8df7b6511b751523f93a354aae1085461867a044
shungo0222 <kimura.shungo@gmail.com>
2023-12-28 18:10:48 +0900

    Fix the `hardhat.config.ts`

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,40 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-contract-sizer";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY as string],
+    },
+    hardhat: {
+      chainId: 31337,
+      initialBaseFeePerGas: 0,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      polygon: `${process.env.POLYGONSCAN_API_KEY}`,
+      polygonMumbai: `${process.env.POLYGONSCAN_API_KEY}`,
+    }
+  },
+  contractSizer: {
+    runOnCompile: true,
+    strict: true,
+    only: ["Escrow"],
+  }
 };
 
 export default config;
