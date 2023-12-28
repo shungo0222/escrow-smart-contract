5cc62c807124606de2aceaec543c6c35c393364c
shungo0222 <kimura.shungo@gmail.com>
2023-12-28 18:24:14 +0900

    Create `deploy.ts`

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,26 +1,84 @@
-import { ethers } from "hardhat";
+import "@nomicfoundation/hardhat-toolbox";
+import hre from "hardhat";
+import { writeFileSync } from "fs";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const [deployer] = await hre.ethers.getSigners();
 
-  const lockedAmount = ethers.parseEther("0.001");
+  console.log("Deploying contracts with the account:", deployer.address);
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  // // トークンの設定
+  // const tokens = [
+  //   {
+  //     name: "USD Coin",
+  //     symbol: "USDC",
+  //     initialSupply: "1000000000", // 1000 USDC, 6桁の小数点以下
+  //     customDecimals: 6,
+  //   },
+  //   {
+  //     name: "Tether USD",
+  //     symbol: "USDT",
+  //     initialSupply: "1000000000", // 1000 USDT, 6桁の小数点以下
+  //     customDecimals: 6,
+  //   },
+  //   {
+  //     name: "JPY Coin",
+  //     symbol: "JPYC",
+  //     initialSupply: "1000000000000000000000", // 1000 JPYC, 18桁の小数点以下
+  //     customDecimals: 18,
+  //   },
+  // ];
 
-  await lock.waitForDeployment();
+  // // 各トークンをデプロイ
+  // for (const token of tokens) {
+  //   const CustomToken = await hre.ethers.getContractFactory("CustomToken");
+  //   const customToken = await CustomToken.deploy(
+  //     token.name,
+  //     token.symbol,
+  //     token.initialSupply,
+  //     token.customDecimals
+  //   );
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+  //   console.log(`${token.name} deployed to: ${(await customToken.getAddress())}`);
+  // }
+
+  // ERC2771Forwarderのデプロイ
+  const Forwarder = await hre.ethers.getContractFactory("ERC2771Forwarder");
+  const forwarder = await Forwarder.deploy("ERC2771Forwarder");
+  const forwarderAddress = await forwarder.getAddress();
+  console.log(`Forwarder deployed to: ${forwarderAddress}`);
+
+  // Escrowコントラクトのデプロイ
+  const Escrow = await hre.ethers.getContractFactory("Escrow");
+  const escrow = await Escrow.deploy(
+    forwarderAddress,
+    1, // minSubmissionDeadlineDays
+    7, // minReviewDeadlineDays
+    7, // minPaymentDeadlineDays
+    270, // lockPeriodDays
+    14  // deadlineExtensionPeriodDays
   );
+  const escrowAddress = await escrow.getAddress();
+  console.log(`Escrow deployed to: ${escrowAddress}`);
+
+  writeFileSync("deploy.mumbai.json", JSON.stringify({
+    ERC2771Forwarder: forwarderAddress,
+    Escrow: escrowAddress,
+  }, null, 2));
+
+  console.log("Waiting for Polygonscan to catch up...");
+  await new Promise(resolve => setTimeout(resolve, 60000)); // Wait 60 seconds
+
+  await hre.run("verify:verify", {
+    address: forwarderAddress,
+    constructorArguments: ["ERC2771Forwarder"],
+  });
+  await hre.run("verify:verify", {
+    address: escrowAddress,
+    constructorArguments: [forwarderAddress, 1, 7, 7, 270, 14],
+  });
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
