18c304a4e7215bd1126e4e1e67e91007e8909984
shungo0222 <kimura.shungo@gmail.com>
2024-02-06 15:25:08 +0900

    Add `polygon` network settings

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,14 @@ import "hardhat-contract-sizer";
 import * as dotenv from "dotenv";
 dotenv.config();
 
+const {
+  POLYGONSCAN_API_KEY,
+  POLYGON_API_KEY,
+  POLYGON_PRIVATE_KEY,
+  MUMBAI_API_KEY,
+  MUMBAI_PRIVATE_KEY
+} = process.env;
+
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.20",
@@ -15,9 +23,13 @@ const config: HardhatUserConfig = {
     }
   },
   networks: {
+    polygon: {
+      url: `https://polygon-mainnet.g.alchemy.com/v2/${POLYGON_API_KEY}`,
+      accounts: [`${POLYGON_PRIVATE_KEY}`],
+    },
     mumbai: {
-      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: [process.env.PRIVATE_KEY as string],
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${MUMBAI_API_KEY}`,
+      accounts: [`${MUMBAI_PRIVATE_KEY}`],
     },
     hardhat: {
       chainId: 31337,
@@ -26,8 +38,8 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: {
-      polygon: `${process.env.POLYGONSCAN_API_KEY}`,
-      polygonMumbai: `${process.env.POLYGONSCAN_API_KEY}`,
+      polygon: `${POLYGONSCAN_API_KEY}`,
+      polygonMumbai: `${POLYGONSCAN_API_KEY}`,
     }
   },
   contractSizer: {
